7394af267b0b9e6816102cf199c27b565fc71e7b
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-01-26 18:57:42 +0000

    'yet to make projects page ready'

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,30 @@
+import { useRef } from 'react'
 import './index.scss'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+    const refForm = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_4w8dwpl',
+                'template_lfizibm',
+                refForm.current,
+                'qj0zEXm6GCMaoGYdh'
+            )
+
+            .then(
+                () => {
+                    alert('Message has been sent! Isaac will get bact to you soon')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send message, kindly try again.')
+                }
+            )
+    }
     return(
         <div className='container contact-page'>
             <div className='text-zone'>
@@ -11,7 +35,7 @@ const Contact = () => {
                     below. I look forward to hearing from you.
                 </p>
                 <div className='contact-form'>
-                    <form>
+                    <form ref={refForm} onSubmit={sendEmail}>
                         <ul>
                             <li className='half'>
                                 <input type='text' name='name' placeholder='FirstName LastName' required />
@@ -37,6 +61,66 @@ const Contact = () => {
 
                         </ul>
                     </form>
+                </div>
+            </div>
+
+            <div className='quote-cube'>
+                <div className='cubespin'>
+                    <div className='face1 face'>
+                        <p>
+                            'We will have eternity to celebrate the victories but only few hours before sunset to win them'
+                            <br/>
+                            <br/>
+                            <span className='quoter'>~ Amy Carmichael</span>
+                        </p>
+                    </div>
+
+                    <div className='face2 face'>
+                        <p>
+                            'Self-education is, I believe, the only kind of education there is.'
+                            <br/>
+                            <br/>
+                            <span className='quoter'>~ Isaac Asimov</span>
+                        </p>
+                    </div>
+
+                    <div className='face3 face'>
+                        <p>
+                            'Life takes a bit of time and a lot of relationship'
+                            <br/>
+                            <br/>
+                            <span className='quoter'>~ William Paul Young</span>
+                        </p>
+                    </div>
+
+                    <div className='face4 face'>
+                        <p>
+                            'It\'s probably my job to tell you life isn\'t fair, but
+                             I figure you already know that. So instead, I\'ll tell you 
+                             that hope is precious, and you\'re right not to give up'
+                            <br/>
+                            <br/>
+                            <span className='quoter'>~ C.J. Redwine</span>
+                        </p>
+                    </div>
+
+                    <div className='face5 face'>
+                        <p >
+                            'For the things we have to learn before we can do them, we learn by doing them.'
+                            <br/>
+                            <br/>
+                            <span className='quoter'>~ Aristotle</span>
+                        </p>
+                    </div>
+
+                    <div className='face6 face'>
+                        <p>
+                            'Let every man be swift to hear, slow to speak, slow to wrath.'
+                            <br/>
+                            <br/>
+                            <span className='quoter'>~ James of the Bible</span>
+                        </p>
+                    </div>
                 </div>
             </div>
 
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,12 @@
+import { faWarning } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
 const Projects = () => {
     return(
-        <div>
-
-
+        <div className='project-page'>
+            <FontAwesomeIcon icon={faWarning} color='#ffd700' />
+            <p> Page Under Construction</p>
         </div>
     )
 }
